9881cea22c2ed4b8441e88b0eead81c22bd07796
sreecharan t <sreecharan.tumuluru@gmail.com>
2023-03-27 19:19:15 +0530

    removed ngnix

--- server_crea.sh
+++ server_crea.sh
@@ -8,4 +8,4 @@ unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 #systemctl restart nginx
-systemctl restart nginx
+systemctl restart 
